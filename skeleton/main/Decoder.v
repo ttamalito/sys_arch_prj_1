7d8901f1790603acfda865c96ee65ec24d317ca7
ttamalito <135988831+ttamalito@users.noreply.github.com>
2023-06-08 16:07:56 +0200

    Add Initial files

---
+++ skeleton/main/Decoder.v
@@ -0,0 +1,97 @@
+module Decoder(
+	input     [31:0] instr,      // Instruction word
+	input            zero,       // Does the current operation in the datapath return 0 as result?
+	output reg       memtoreg,   // Use the loaded word instead of the ALU result as result
+	output reg       memwrite,   // Write to the data memory
+	output reg       dobranch,   // Perform a relative jump
+	output reg       alusrcbimm, // Use the immediate value as second operand
+	output reg [4:0] destreg,    // Number of the target register to (possibly) be written
+	output reg       regwrite,   // Write to the target register
+	output reg       dojump,     // Perform an absolute jump
+	output reg [2:0] alucontrol  // ALU control bits
+);
+	// Extract the primary and secondary opcode
+	wire [5:0] op = instr[31:26];
+	wire [5:0] funct = instr[5:0];
+
+	always @*
+	begin
+		case (op)
+			6'b000000: // R-type instruction
+				begin
+					regwrite = 1;
+					destreg = instr[15:11];
+					alusrcbimm = 0;
+					dobranch = 0;
+					memwrite = 0;
+					memtoreg = 0;
+					dojump = 0;
+					case (funct)
+						6'b100001: alucontrol = // TODO // addition unsigned
+						6'b100011: alucontrol = // TODO // subtraction unsigned
+						6'b100100: alucontrol = // TODO // and
+						6'b100101: alucontrol = // TODO // or
+						6'b101011: alucontrol = // TODO // set-less-than unsigned
+						default:   alucontrol = // TODO // undefined
+					endcase
+				end
+			6'b100011, // Load data word from memory
+			6'b101011: // Store data word
+				begin
+					regwrite = ~op[3];
+					destreg = instr[20:16];
+					alusrcbimm = 1;
+					dobranch = 0;
+					memwrite = op[3];
+					memtoreg = 1;
+					dojump = 0;
+					alucontrol = // TODO // Effective address: Base register + offset
+				end
+			6'b000100: // Branch Equal
+				begin
+					regwrite = 0;
+					destreg = 5'bx;
+					alusrcbimm = 0;
+					dobranch = zero; // Equality test
+					memwrite = 0;
+					memtoreg = 0;
+					dojump = 0;
+					alucontrol = // TODO // Subtraction
+				end
+			6'b001001: // Addition immediate unsigned
+				begin
+					regwrite = 1;
+					destreg = instr[20:16];
+					alusrcbimm = 1;
+					dobranch = 0;
+					memwrite = 0;
+					memtoreg = 0;
+					dojump = 0;
+					alucontrol = // TODO // Addition
+				end
+			6'b000010: // Jump immediate
+				begin
+					regwrite = 0;
+					destreg = 5'bx;
+					alusrcbimm = 0;
+					dobranch = 0;
+					memwrite = 0;
+					memtoreg = 0;
+					dojump = 1;
+					alucontrol = // TODO
+				end
+			default: // Default case
+				begin
+					regwrite = 1'bx;
+					destreg = 5'bx;
+					alusrcbimm = 1'bx;
+					dobranch = 1'bx;
+					memwrite = 1'bx;
+					memtoreg = 1'bx;
+					dojump = 1'bx;
+					alucontrol = // TODO
+				end
+		endcase
+	end
+endmodule
+
---
+++ skeleton/warmup/MealyMachine.v
@@ -0,0 +1,124 @@
+module MealyPattern(
+	input        clock,
+	input        i,
+	output [1:0] o
+);
+
+// define the registers to store 010 and 101
+// 2bit register to store 010 (name of the machine is 010)
+reg [1:0] zero_one_zero;
+// 2-bit register to store 101 (name of the machine is 010)
+reg [1:0] one_zero_one;
+
+// set the values to zero of the registers
+initial begin
+	zero_one_zero[0] = 'b0;
+	zero_one_zero[1] = 'b0;
+	one_zero_one[0] = 'b0;
+	one_zero_one[1] = 'b0;
+	end 
+// logic to store in Q1 for 010
+// note Q1 == reg[0] i.e., the least significant bit
+wire w1;
+assign w1 = i & (~zero_one_zero[0]); // not Q1 AND input
+wire w2;
+assign w2 = w1 & zero_one_zero[1]; // w1 AND Q2
+
+//logic to compute Q2 for zero_one_zero
+wire w3;
+assign w3 = ~i & (~zero_one_zero[0]); // NOT input AND NOT Q1
+wire w4;
+assign w4 =  ~i & (~zero_one_zero[1]); // NOT input AND NOT Q2
+wire w5;
+assign w5 = w3 | w4; // w3 OR w4
+
+// logic for the output 
+wire w6;
+assign w6 =  ~i & (zero_one_zero[0]); // NOT input AND Q1
+
+
+
+// now the logic for 101... pretty similar
+wire w7, w8, w9, w10, w11, w12;
+assign w7 = ~i & (~one_zero_one[0]); // NOT input AND NOT Q1
+assign w8 = w7 & one_zero_one[1]; // w7 AND Q2
+assign w9 = i & (~one_zero_one[0]); // input AND NOT Q1
+assign w10 = i & (~one_zero_one[1]); // input AND NOT Q2
+assign w11 = w9 | w10; // w9 OR w10
+assign w12 = i & (one_zero_one[0]); // input AND Q1
+
+
+// now define the sequential circuit
+
+always @ (posedge clock) begin
+	// modify the states of the registers accordingly 
+	// w2 has the new value for Q1 for zero_one_zero
+	zero_one_zero[0] <= w2;
+	// w5 has the new value for Q2 for zero_one_zero
+	zero_one_zero[1] <= w5;
+	// w8 has the new value for Q1 for one_zero_one
+	one_zero_one[0] <= w8;
+	// w11 has the new value for Q2 for one_zero_one
+	one_zero_one[1] <= w11;
+	end
+	// the output
+assign o[1] = w6 & (~zero_one_zero[1]); // w6 AND NOT Q2
+	// the final output 
+assign o[0] = w12 & (~one_zero_one[1]); // w12 AND NOT Q2
+endmodule
+
+module MealyPatternTestbench();
+
+	// define the inputs
+	reg clock, i;
+	wire [1:0] o;
+
+	MealyPattern machine(.clock(clock), .i(i), .o(o));
+
+	//initial stae
+	initial 
+	begin
+		clock = 0;
+		i =0;
+		//#2;
+		//i = 0;
+		#2;
+		i = 1;
+		#2;
+		i = 1;
+		#2;
+		i = 0;
+		#2;
+		i = 1;
+		#2;
+		i = 0;
+		#2;
+		i = 1;	
+		#2;
+		i = 0;
+		#2;
+		i = 1;
+		#2;
+		i = 1;
+		$finish;
+	end
+	
+	always begin
+		#1;
+		clock = !clock;
+	end
+	
+	initial 
+	begin 
+		$dumpfile ("mealy.vcd");
+		$dumpvars;
+	end
+	
+initial 
+	begin 
+		$display("\t\ttime ,\tclock ,\ti, \to ");
+		$monitor("%d ,\t%b ,\t%b ,\t%b " ,$time, clock, i, o);
+	end 
+		
+endmodule
+
